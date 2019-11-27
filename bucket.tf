308882d9afdec9634221f6c0fd42cb23aed5973f
Toby Clemson <tobyclemson@gmail.com>
2019-11-27 12:28:47 +0000

    Remove comma.

--- bucket.tf
+++ bucket.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "infrastructure_events" {
   region = var.region
 
   tags {
-    Name = "${var.bucket_name_prefix}-${var.region}-${var.deployment_identifier}",
+    Name = "${var.bucket_name_prefix}-${var.region}-${var.deployment_identifier}"
     Component = "common"
     DeploymentIdentifier = var.deployment_identifier
   }
