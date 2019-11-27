5dacd260b74747ab4fbc591ed985358a3db3c787
Toby Clemson <tobyclemson@gmail.com>
2019-11-27 12:35:17 +0000

    Fix terraform 12 issues.

--- bucket.tf
+++ bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "infrastructure_events" {
   bucket = "${var.bucket_name_prefix}-${var.region}-${var.deployment_identifier}"
   region = var.region
 
-  tags {
+  tags = {
     Name = "${var.bucket_name_prefix}-${var.region}-${var.deployment_identifier}"
     Component = "common"
     DeploymentIdentifier = var.deployment_identifier
